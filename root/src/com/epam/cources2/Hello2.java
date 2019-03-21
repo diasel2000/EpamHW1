646ab60af3c8911f88bdcbee866961b0f4aa1cf2
Anatolii Huzov <n221296@gmail.com>
2019-03-22 01:34:59 +0200

    add root

---
+++ root/src/com/epam/cources2/Hello2.java
@@ -0,0 +1,10 @@
+package com.epam.cources2;
+
+import com.epam.cources.*;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("I am Hello2");
+        Hello print = new Hello();
+    }
+}
